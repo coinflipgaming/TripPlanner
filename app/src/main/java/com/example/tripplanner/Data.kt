b657d00e476b1fbf0c6bb9070131d512cb62d925
yomama <palg00mesmieciu@gmail.com>
2022-12-09 11:30:11 +0100

    layout skelton

---
+++ app/src/main/java/com/example/tripplanner/Data.kt
@@ -0,0 +1,7 @@
+package com.example.tripplanner
+
+class Data {
+    companion object{
+
+    }
+}
---
+++ app/src/main/java/com/example/tripplanner/TripList.kt
@@ -0,0 +1,11 @@
+package com.example.tripplanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TripList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trip_list)
+    }
+}
